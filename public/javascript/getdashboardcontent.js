b3fd28216c78c03a86210f87f43898e8a3ac40d0
zeadam <adzegar@gmail.com>
2022-12-21 20:17:07 +0100

    added marks table - still in development

--- public/javascript/getdashboardcontent.js
+++ public/javascript/getdashboardcontent.js
@@ -24,7 +24,65 @@ function getmarks() { //we'll temporarily work with the current trimester
     xhr.open('POST', `/api/content`, true);
     xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     xhr.onload = function(e) { 
-      console.log(JSON.parse(xhr.response));
+        let data = JSON.parse(xhr.response);
+        const subjects = data.data.marks.subjects;
+        // Create the table element
+        const table = document.getElementById('marks-table');
+
+        // Iterate over the subjects array
+        for (const subject of subjects) {
+        // Create the thead element
+        const thead = document.createElement('thead');
+        // Create the tr element
+        const tr = document.createElement('tr');
+
+        // Create the th element for the subject name
+        const thSubject = document.createElement('th');
+        thSubject.innerHTML = subject.name;
+
+        // Create the th element for the averages
+        const thAverages = document.createElement('th');
+        thAverages.innerHTML = `${subject.averages.student} (class avg: ${subject.averages.studentClass})`;
+
+        // Append the th elements to the tr element
+        tr.appendChild(thSubject);
+        tr.appendChild(thAverages);
+
+        // Append the tr element to the thead element
+        thead.appendChild(tr);
+
+        // Create the tbody element
+        const tbody = document.createElement('tbody');
+
+        // Iterate over the marks array
+        for (const mark of subject.marks) {
+            // Create the tr element
+            const tr = document.createElement('tr');
+
+            // Create the td element for the mark title
+            const tdTitle = document.createElement('td');
+            tdTitle.innerHTML = mark.title;
+
+            // Create the td element for the mark value and scale
+            const tdValue = document.createElement('td');
+            tdValue.innerHTML = `${mark.value}/${mark.scale} (coeff. ${mark.coefficient})`;
+
+            // Append the td elements to the tr element
+            tr.appendChild(tdTitle);
+            tr.appendChild(tdValue);
+
+            // Append the tr element to the tbody element
+            tbody.appendChild(tr);
+        }
+
+        // Append the thead and tbody elements to the table element
+        table.appendChild(thead);
+        table.appendChild(tbody);
+        }
+
+        // Append the table element to the desired location in the DOM
+        document.body.appendChild(table);
+
     }
 xhr.send(JSON.stringify(body));
 
